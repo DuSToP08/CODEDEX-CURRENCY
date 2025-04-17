39cb6e85c8a331a330671d7bcf1a86d50510b96a
DuSToP08 <DA08GZ@GMAIL.COM>
2025-04-16 21:26:13 -0700

    Create codex_converter.py

---
+++ codex_converter.py
@@ -0,0 +1,14 @@
+# Write code below 💖
+
+
+pesos = int(input('What do you have left in pesos? '))
+soles = float(input('What do you have left in soles? '))
+reais = float(input('What do you have left in reais? '))
+
+P_usd_conv = pesos * .00023
+S_usd_conv = soles * .27
+R_usd_conv = reais * .17
+
+total_usd = P_usd_conv + S_usd_conv + R_usd_conv
+
+print(total_usd)
